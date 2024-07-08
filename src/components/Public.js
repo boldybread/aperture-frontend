19ed7cda7f1ed15c5b99fde29dd67357e30f68f3
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-07-08 13:58:12 +1000

    feat: created tables for user/notes & changed favicon & other minor fixes

--- src/components/Public.js
+++ src/components/Public.js
@@ -7,7 +7,9 @@ const Public = () => {
                 <h1>Welcome to the <span className="nowrap">Aperture Science Task Management System</span></h1>
             </header>
             <main className="public__main">
-                <p>Aperture Laboraties is a subsiduary of Aperture Science, Inc a United States based scientific research corporation. Formally trading as Aperture Fixtures a shower curtain manufacturer the organisation evolved and moved into the theoretical physics research realm. This is the Aperture Science Task Management System where research tasks will be assigned to current and future employees!</p>
+                <p>Aperture Laboraties is a subsiduary of Aperture Science, Inc a United States based scientific research corporation. Formally trading as Aperture Fixtures a shower curtain manufacturer the organisation evolved and moved into the theoretical physics research realm. 
+                <br /><br />
+                This is the Aperture Science Task Management System where research tasks will be assigned to current and future employees!</p>
                 <address className="public__addr">
                     Aperture Science Enrichment Center<br />
                     Upper Michigan<br />
