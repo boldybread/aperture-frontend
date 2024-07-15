56dfc887c513e1d2dfa64169a586b42340c2e33d
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-07-15 14:56:30 +1000

    minor changes/fixes

--- src/components/Public.js
+++ src/components/Public.js
@@ -11,7 +11,11 @@ const Public = () => {
                 <br /><br />
                 This is the Aperture Science Task Management System where research tasks will be assigned to current and future employees!
                 <br /><br />
-                You are here because you display the ideal characteristics of a test candidate, you are currently alive and contain equal to or greater than one limb. Congratulations!</p>
+                You are here because you display the ideal characteristics of a test candidate, you are currently alive and contain equal to or greater than two limbs! Congratulations!
+                <br /><br />
+                Exemplary employees don't require help. If this is you please login to commence testing. 
+                <br /><br />
+                For help please follow this <a href="https://valvearchive.com/web_archive/aperturescience.com/2007-10-17.html">help</a> link and type help and know that management is disappointed in you.</p>
                 <address className="public__addr">
                     Aperture Science Enrichment Center<br />
                     Upper Michigan<br />
