b2b06e4f8f799fb2d58cf88feb9cb61d78c0e051
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-07-22 09:06:55 +1000

    feat: Added permissions and roles. minor error debug

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 const Welcome = () => {
+
+    const { username, isManager, isAdmin } = useAuth()
 
     const date = new Date()
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
@@ -10,15 +13,15 @@ const Welcome = () => {
 
             <p>{today}</p>
 
-            <h1>Welcome!</h1>
+            <h1>Welcome {username}!</h1>
 
             <p><Link to="/dash/notes">View Task Management System</Link></p>
 
             <p><Link to="/dash/notes/new">Add New Task</Link></p>
 
-            <p><Link to="/dash/users">View User Settings</Link></p>
+            {(isManager || isAdmin) && <p><Link to="/dash/users">View User Settings</Link></p>}
 
-            <p><Link to="/dash/users/new">Add New User</Link></p>
+            {(isManager || isAdmin) && <p><Link to="/dash/users/new">Add New User</Link></p>}
 
         </section>
     )
