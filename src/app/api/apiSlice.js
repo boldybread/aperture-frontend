b90ddf4f89771e0ee7a0b87e66bd0295f2c6f820
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-07-22 13:57:05 +1000

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://aperturescience.netlify.app',
+    baseUrl: 'https://aperturescience-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
